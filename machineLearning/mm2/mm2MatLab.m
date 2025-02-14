ccfb0f00a5a60f62e1954cf8d53cc7049226e206
MhawnSendes <jtheil22@student.aau.dk>
2025-02-14 09:46:19 +0100

    i am giving up on matlab at this point

--- machineLearning/mm2/mm2MatLab.m
+++ machineLearning/mm2/mm2MatLab.m
@@ -1,4 +1,21 @@
-A = [1/5 -1/3
-     1/20 1/5];
-Hf = (A.'*A).'+ A.'*A
-eig(Hf)
+clc; clear; close all;
+syms X [3 1] matrixjthei  % Define symbolic variables
+
+% Define matrix A and vector b
+A = [1/5, -1/3; 1/20, 3/5];
+b = [1; 4];
+
+% Define function f(x)
+f =x' * A' * A * x + b' * x;
+diff(f,x,x')
+% Compute gradient
+g = gradient(f,x);
+
+% Compute Hessian
+H = hessian(f, x);
+
+% Display results
+disp('Gradient g(x):');
+disp(g);
+disp('Hessian Hf(x):');
+disp(H);
